a6fae1a64636541853ae0f5fbc635819287ec585
iznauy <zy05160516@126.com>
2018-10-24 17:42:28 +0800

    init project

---
+++ src/main/java/top/iznauy/chinamobile/ChinamobileApplication.java
@@ -0,0 +1,12 @@
+package top.iznauy.chinamobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinamobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinamobileApplication.class, args);
+	}
+}
---
+++ src/test/java/top/iznauy/chinamobile/ChinamobileApplicationTests.java
@@ -0,0 +1,16 @@
+package top.iznauy.chinamobile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChinamobileApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
